02c937f1cd6bda424b0538c0ccd84bfc93b0b4ec
Ole Hansen <ole@jlab.org>
2022-08-24 11:38:17 -0400

    Version is 2.3

--- panguin.cc
+++ panguin.cc
@@ -8,7 +8,7 @@
 #include <ctime>
 #include <string>
 
-#define PANGUIN_VERSION "Panguin version 2.2 (24-Aug-2022)"
+#define PANGUIN_VERSION "Panguin version 2.3 (24-Aug-2022)"
 
 using namespace std;
 
