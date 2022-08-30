66030f6e70d537356430ad0b6e674b4418690c4f
Ole Hansen <ole@jlab.org>
2022-08-30 18:44:06 -0400

    Catch exceptions at top level

--- panguin.cc
+++ panguin.cc
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <ctime>
 #include <string>
+#include <stdexcept>
 
 #define PANGUIN_VERSION "Panguin version 2.1 (24-Aug-2022)"
 
@@ -24,71 +25,77 @@ int main( int argc, char** argv )
   bool printonly{false};
   bool saveImages{false};
 
-  CLI::App cli("panguin: configurable ROOT data visualization tool");
+  try {
+    CLI::App cli("panguin: configurable ROOT data visualization tool");
 
-  cli.add_option("-f,--config-file", cfgfile,
-                 "Job configuration file")
-    ->capture_default_str()->type_name("<file name>");
-  cli.add_option("-r,--run", run,
-                 "Run number")
-    ->type_name("<run number>");
-  cli.add_option("-R,--root-file", rootfile,
-                 "ROOT file to process")
-    ->type_name("<file name>");
-  cli.add_flag("-P,-b,--batch", printonly,
-               "No GUI. Save plots to summary file(s)");
-  cli.add_option("-E,--plot-format", plotfmt,
-                 "Plot format (pdf, png, jpg ...)")
-    ->type_name("<fmt>");
-  cli.add_option("-C,--config-dir", cfgdir,
-                 "Search path for configuration files & macros "
-                 "(\":\"-separated)")
-    ->type_name("<path>");
-  cli.add_option("--root-dir", rootdir,
-                 "ROOT files search path (\":\"-separated)")
-    ->type_name("<path>");
-  cli.add_option("-O,--plots-dir", pltdir,
-                 "Output directory for summary plots")
-    ->type_name("<dir>");
-  cli.add_flag("-I,--images", saveImages,
-               "Save individual plots as images (implies -P)");
-  cli.add_option("-F,--image-format", imgfmt,
-                 "Image file format (png, jpg ...)")
-    ->type_name("<fmt>");
-  cli.add_option("-H,--images-dir", imgdir,
-                 "Output directory for individual images (default: plots-dir)")
-    ->type_name("<dir>");
-  cli.add_option("-v,--verbosity", verbosity,
-                 "Set verbosity level (>=0)")
-    ->type_name("<level>");
-  cli.set_version_flag("-V,--version", PANGUIN_VERSION);
+    cli.add_option("-f,--config-file", cfgfile,
+                   "Job configuration file")
+      ->capture_default_str()->type_name("<file name>");
+    cli.add_option("-r,--run", run,
+                   "Run number")
+      ->type_name("<run number>");
+    cli.add_option("-R,--root-file", rootfile,
+                   "ROOT file to process")
+      ->type_name("<file name>");
+    cli.add_flag("-P,-b,--batch", printonly,
+                 "No GUI. Save plots to summary file(s)");
+    cli.add_option("-E,--plot-format", plotfmt,
+                   "Plot format (pdf, png, jpg ...)")
+      ->type_name("<fmt>");
+    cli.add_option("-C,--config-dir", cfgdir,
+                   "Search path for configuration files & macros "
+                   "(\":\"-separated)")
+      ->type_name("<path>");
+    cli.add_option("--root-dir", rootdir,
+                   "ROOT files search path (\":\"-separated)")
+      ->type_name("<path>");
+    cli.add_option("-O,--plots-dir", pltdir,
+                   "Output directory for summary plots")
+      ->type_name("<dir>");
+    cli.add_flag("-I,--images", saveImages,
+                 "Save individual plots as images (implies -P)");
+    cli.add_option("-F,--image-format", imgfmt,
+                   "Image file format (png, jpg ...)")
+      ->type_name("<fmt>");
+    cli.add_option("-H,--images-dir", imgdir,
+                   "Output directory for individual images (default: plots-dir)")
+      ->type_name("<dir>");
+    cli.add_option("-v,--verbosity", verbosity,
+                   "Set verbosity level (>=0)")
+      ->type_name("<level>");
+    cli.set_version_flag("-V,--version", PANGUIN_VERSION);
 
-  CLI11_PARSE(cli, argc, argv);
+    CLI11_PARSE(cli, argc, argv);
 
-  if( saveImages ) {
-    printonly = true;
-    if( imgdir.empty() )
-      imgdir = pltdir;
+    if( saveImages ) {
+      printonly = true;
+      if( imgdir.empty() )
+        imgdir = pltdir;
+    }
+
+    if( verbosity <= 0 ) {
+      verbosity = 0;
+    } else if( verbosity > 0 ) {
+      cout << cli.config_to_str(true, false);
+    }
+
+    if( !gSystem->AccessPathName("./rootlogon.C") ) {
+      gROOT->ProcessLine(".x rootlogon.C");
+    }
+
+    if( !gSystem->AccessPathName("~/rootlogon.C") ) {
+      gROOT->ProcessLine(".x ~/rootlogon.C");
+    }
+
+    TApplication theApp("panguin2", &argc, argv, nullptr, -1);
+    online({cfgfile, cfgdir, rootfile, rootdir, plotfmt, imgfmt, pltdir,
+            imgdir, run, verbosity, printonly, saveImages});
+    theApp.Run();
+
+  } catch ( const exception& e ) {
+    cerr << "Error while running panguin: " << e.what() << endl;
+    return 1;
   }
-
-  if( verbosity <= 0 ) {
-    verbosity = 0;
-  } else if( verbosity > 0 ) {
-    cout << cli.config_to_str(true, false);
-  }
-
-  if( !gSystem->AccessPathName("./rootlogon.C") ) {
-    gROOT->ProcessLine(".x rootlogon.C");
-  }
-
-  if( !gSystem->AccessPathName("~/rootlogon.C") ) {
-    gROOT->ProcessLine(".x ~/rootlogon.C");
-  }
-
-  TApplication theApp("panguin2", &argc, argv, nullptr, -1);
-  online({cfgfile, cfgdir, rootfile, rootdir, plotfmt, imgfmt, pltdir, imgdir,
-          run, verbosity, printonly, saveImages});
-  theApp.Run();
 
   return 0;
 }
