JeffersonLab/panguin

commit 4b22baf1e867fd1d01eb1879849dad70eea50823

Actually return to and exit program from main
Original code had various exit points via gApplication->Terminate(), which is
rather brute-force.

Also, fix some code lint.

src/panguinOnline.cc
@@ -4,14 +4,6 @@
 ///////////////////////////////////////////////////////////////////
 
 #include "panguinOnline.hh"
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <list>
-#include <sys/stat.h>
-#include <ctime>
-#include <TMath.h>
 #include <TBranch.h>
 #include <TGClient.h>
 #include <TCanvas.h>
@@ -27,16 +19,29 @@
 #include "TEnv.h"
 #include "TRegexp.h"
 #include "TGaxis.h"
+#include <string>
 #include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <sys/stat.h>
+#include <ctime>
 #include <utility>
 #include <cassert>
 #include <memory>
+#include <type_traits>  // std::make_signed
 
 #define OLDTIMERUPDATE
 
 ClassImp(OnlineGUI)
 
 using namespace std;
+
+template<typename T>
+static inline
+typename std::make_signed<T>::type SINT(T uint) {
+  return static_cast<typename std::make_signed<T>::type>(uint);
+}
 
 //_____________________________________________________________________________
 // Helper function to get value for given key from const std::map&
@@ -140,10 +145,11 @@ string OnlineGUI::SubstitutePlaceholders( string str, const string& var ) const
 //  Class: OnlineGUI
 //
 //    Creates a GUI to display the commands used in OnlineConfig
-//
+//    unless batch mode is set, i.e. config.DoPrintOnly() == true
+//    in which case the caller should invoke PrintPages().
 //
 
-OnlineGUI::OnlineGUI( OnlineConfig&& config )
+OnlineGUI::OnlineGUI( OnlineConfig config )
   : fConfig{std::move(config)}
   , runNumber{0}
   , current_page{0}
@@ -166,15 +172,9 @@ OnlineGUI::OnlineGUI( OnlineConfig&& config )
     }
   }
 
-  if( fPrintOnly ) {
-    PrintPages();
-  } else {
+  if( !fPrintOnly )
     CreateGUI(gClient->GetRoot(), 1600, 1200);
-  }
 }
-
-OnlineGUI::OnlineGUI( const OnlineConfig& config )
-  : OnlineGUI(std::move(OnlineConfig(config))) {}
 
 void OnlineGUI::CreateGUI( const TGWindow* p, UInt_t w, UInt_t h )
 {
@@ -266,7 +266,7 @@ void OnlineGUI::CreateGUI( const TGWindow* p, UInt_t w, UInt_t h )
   fPageListBox->IntegralHeight(kTRUE);
 
   TString buff;
-  for( UInt_t i = 0; i < fConfig.GetPageCount(); i++ ) {
+  for( Int_t i = 0; i < SINT(fConfig.GetPageCount()); ++i ) {
     buff = fConfig.GetPageTitle(i);
     fPageListBox->AddEntry(buff, i);
   }
@@ -464,7 +464,7 @@ void OnlineGUI::DoDraw()
   //keys are "variable", "cut", "drawopt", "title", "treename", "grid", "nostat"
 
   // Draw the histograms.
-  for( UInt_t i = 0; i < draw_count; i++ ) {
+  for( Int_t i = 0; i < SINT(draw_count); i++ ) {
     current_pad = i + 1;
     fConfig.GetDrawCommand(current_page, current_pad - 1, drawcommand);
     fCanvas->cd(current_pad);
@@ -1233,32 +1233,32 @@ void OnlineGUI::PrintToFile()
   fi.fIniDir = StrDup(dir.Data());
 
   new TGFileDialog(gClient->GetRoot(), fMain, kFDSave, &fi);
-  if( fi.fFilename ) fCanvas->Print(fi.fFilename);
+  if( fi.fFilename )
+    fCanvas->Print(fi.fFilename);
 }
 
-void OnlineGUI::PrintPages()
+Int_t OnlineGUI::PrintPages()
 {
   // Routine to go through each defined page, and print the output to
   // a postscript file. (good for making sample histograms).
+  // Returns 0 on success, 1 on error.
 
   // Open the RootFile
-  //  unless we're watching a file.
   fRootFile = new TFile(fConfig.GetRootFile(), "READ");
   if( !fRootFile->IsOpen() ) {
     cerr << "ERROR:  rootfile: " << fConfig.GetRootFile()
          << " cannot be opened"
          << endl;
-    gApplication->Terminate();
-  } else {
-    fFileAlive = kTRUE;
-    runNumber = fConfig.GetRunNumber();
-    GetFileObjects();
-    GetRootTree();
-    GetTreeVars();
-    for( UInt_t i = 0; i < fRootTree.size(); i++ ) {
-      if( !fRootTree[i] ) {
-        fRootTree.erase(fRootTree.begin() + i);
-      }
+    return 1;
+  }
+  fFileAlive = kTRUE;
+  runNumber = fConfig.GetRunNumber();
+  GetFileObjects();
+  GetRootTree();
+  GetTreeVars();
+  for( UInt_t i = 0; i < fRootTree.size(); i++ ) {
+    if( !fRootTree[i] ) {
+      fRootTree.erase(fRootTree.begin() + i);
     }
   }
   TString goldenfilename = fConfig.GetGoldenFile();
@@ -1297,7 +1297,7 @@ void OnlineGUI::PrintPages()
     filename = SubstitutePlaceholders(protofilename);
     auto outdir = DirnameStr(filename.Data());
     if( MakePlotsDir(outdir) )
-      gApplication->Terminate();
+      return 1;
   }
 
   TString pagehead = "Summary Plots";
@@ -1314,8 +1314,9 @@ void OnlineGUI::PrintPages()
   gStyle->SetPadBorderMode(0);
   //gStyle->SetHistLineColor(1);
   gStyle->SetHistFillStyle(0);
-  if( !pagePrint ) fCanvas->Print(filename + "[");
-  for( UInt_t i = 0; i < fConfig.GetPageCount(); i++ ) {
+  if( !pagePrint )
+    fCanvas->Print(filename + "[");
+  for( Int_t i = 0; i < SINT(fConfig.GetPageCount()); i++ ) {
     current_page = i;
     DoDraw();
     TString pagename = pagehead;
@@ -1331,69 +1332,72 @@ void OnlineGUI::PrintPages()
            << " to file = " << filename << endl;
       auto outdir = DirnameStr(filename.Data());
       if( MakePlotsDir(outdir) )
-        gApplication->Terminate();
+        return 1;
     }
     fCanvas->Print(filename);
   }
-  if( !pagePrint ) fCanvas->Print(filename + "]");
+  if( !pagePrint )
+    fCanvas->Print(filename + "]");
 
-  gApplication->Terminate();
+  return 0;
+}
+
+//_____________________________________________________________________________
+template<typename PTR> inline void DelPtr( PTR*& p )
+{
+  delete p; p = nullptr;
+}
+
+//_____________________________________________________________________________
+void OnlineGUI::DeleteGUI()
+{
+  DelPtr(timer);
+  DelPtr(timerNow);
+  DelPtr(fPrint);
+  DelPtr(fExit);
+  DelPtr(fRunNumber);
+  DelPtr(fPrev);
+  DelPtr(fNext);
+  DelPtr(wile);
+  DelPtr(fNow);
+  DelPtr(fRootFileLastUpdated);
+  DelPtr(fPageListBox);
+  DelPtr(hframe);
+  DelPtr(fEcanvas);
+  DelPtr(vframe);
+  DelPtr(fBottomFrame);
+  DelPtr(fTopframe);
+  DelPtr(fMain);
 }
 
 //_____________________________________________________________________________
 void OnlineGUI::MyCloseWindow()
 {
-  fMain->SendCloseMessage();
   cout << "OnlineGUI Closed." << endl;
   if( timer ) {
     timer->Stop();
-    delete timer;
   }
-  delete fPrint;
-  delete fExit;
-  delete fRunNumber;
-  delete fPrev;
-  delete fNext;
-  delete wile;
-  delete fPageListBox;
-  delete hframe;
-  delete fEcanvas;
-  delete fBottomFrame;
-  delete vframe;
-  delete fTopframe;
-  delete fMain;
-  delete fGoldenFile;
-  delete fRootFile;
+  DeleteGUI();
 
   gApplication->Terminate();
 }
 
+//_____________________________________________________________________________
 void OnlineGUI::CloseGUI()
 {
   // Routine to take care of the Exit GUI button
   fMain->SendCloseMessage();
 }
 
+//_____________________________________________________________________________
 OnlineGUI::~OnlineGUI()
 {
-  //  fMain->SendCloseMessage();
-  if( timer ) {
+  if( timer )
     timer->Stop();
-    delete timer;
+  if( fMain ) {
+    fMain->SendCloseMessage();
+    DeleteGUI();
   }
-  delete fPrint;
-  delete fExit;
-  delete fRunNumber;
-  delete fPrev;
-  delete fNext;
-  delete wile;
-  delete fPageListBox;
-  delete hframe;
-  delete fEcanvas;
-  delete vframe;
-  delete fBottomFrame;
-  delete fTopframe;
-  delete fMain;
-  delete fGoldenFile;
-  delete fRootFile;
+  DelPtr(fGoldenFile);
+  DelPtr(fRootFile);
 }

include/panguinOnline.hh
@@ -71,11 +71,11 @@ class OnlineGUI {
 
   std::string SubstitutePlaceholders(
     std::string str, const std::string& var = std::string() ) const;
+  void DeleteGUI();
 
 public:
   using cmdmap_t = std::map<std::string, std::string>;
-  explicit OnlineGUI( const OnlineConfig& config );
-  explicit OnlineGUI( OnlineConfig&& config );
+  explicit OnlineGUI( OnlineConfig config );
   void CreateGUI( const TGWindow* p, UInt_t w, UInt_t h );
   virtual ~OnlineGUI();
   void DoDraw();
@@ -84,7 +84,8 @@ public:
   void DoListBox( Int_t id );
   void CheckPageButtons();
   // Specific Draw Methods
-  Bool_t IsHistogram( const TString& );
+  Bool_t IsHistogram( const TString& objectname );
+  Bool_t IsPrintOnly() const { return fPrintOnly; }
   void GetFileObjects();
   void GetTreeVars();
   void GetRootTree();
@@ -104,7 +105,7 @@ public:
   void CheckRootFile();
   Int_t OpenRootFile();
   void PrintToFile();
-  void PrintPages();
+  Int_t PrintPages();
   void MyCloseWindow();
   void CloseGUI();
   void SetVerbosity( int ver ) { fVerbosity = ver; }

panguin.cc
@@ -5,15 +5,14 @@
 #include <TROOT.h>
 #include <TSystem.h>
 #include <iostream>
-#include <ctime>
-#include <string>
 #include <stdexcept>
+#include <memory>
 
 #define PANGUIN_VERSION "Panguin version 2.4 (07-Oct-2022)"
 
 using namespace std;
 
-void online( const OnlineConfig::CmdLineOpts& opts );
+unique_ptr<OnlineGUI> online( const OnlineConfig::CmdLineOpts& opts );
 
 int main( int argc, char** argv )
 {
@@ -68,7 +67,7 @@ int main( int argc, char** argv )
       ->type_name("<level>");
     cli.set_version_flag("-V,--version", PANGUIN_VERSION);
 
-    CLI11_PARSE(cli, argc, argv);
+    CLI11_PARSE(cli, argc, argv)
 
     if( saveImages ) {
       printonly = true;
@@ -91,9 +90,16 @@ int main( int argc, char** argv )
     }
 
     TApplication theApp("panguin2", &argc, argv, nullptr, -1);
-    online({cfgfile, cfgdir, rootfile,  goldenfile, rootdir, plotfmt, imgfmt,
-            pltdir, imgdir, run, verbosity, printonly, saveImages});
-    theApp.Run();
+    auto gui
+      = online({cfgfile, cfgdir, rootfile, goldenfile, rootdir, plotfmt,
+                imgfmt, pltdir, imgdir, run, verbosity, printonly,
+                saveImages});
+    if( gui ) {
+      if( gui->IsPrintOnly() )
+        gui->PrintPages();
+      else
+        theApp.Run(true);
+    }
 
   } catch ( const exception& e ) {
     cerr << "Error while running panguin: " << e.what() << endl;
@@ -104,7 +110,7 @@ int main( int argc, char** argv )
 }
 
 
-void online( const OnlineConfig::CmdLineOpts& opts )
+unique_ptr<OnlineGUI> online( const OnlineConfig::CmdLineOpts& opts )
 {
 
   if( opts.printonly ) {
@@ -115,7 +121,7 @@ void online( const OnlineConfig::CmdLineOpts& opts )
 
   OnlineConfig fconfig(opts);
   if( !fconfig.ParseConfig() )
-    gApplication->Terminate();
+    return nullptr;
 
   TString macropath = gROOT->GetMacroPath();
   macropath += ":./macros";   // for backward compatibility
@@ -124,5 +130,9 @@ void online( const OnlineConfig::CmdLineOpts& opts )
     macropath = ".:" + guipath + ":" + macropath;
   gROOT->SetMacroPath(macropath);
 
-  new OnlineGUI(std::move(fconfig));
+#if __cplusplus >= 201402L
+  return make_unique<OnlineGUI>(std::move(fconfig));
+#else
+  return unique_ptr<OnlineGUI>(new OnlineGUI(std::move(fconfig)));
+#endif
 }

src/panguinOnlineConfig.cc
@@ -9,6 +9,7 @@
 #include <iomanip>    // quoted, setw, setfill
 #include <cctype>     // isalnum, isdigit
 #include <algorithm>  // find_if
+#include <type_traits>// make_signed
 #include <sys/stat.h>
 #if __cplusplus >= 201703L
 #include <regex>
@@ -140,7 +141,8 @@ static string ExpandFileName( string str )
   if( str.size() < 2 )
     return str;
   while( (pos = str.find('$')) != string::npos ) {
-    auto iend = find_if(str.begin() + pos + 1, str.end(), []( int c ) {
+    auto spos = static_cast<make_signed<decltype(pos)>::type>(pos);
+    auto iend = find_if(str.begin() + spos + 1, str.end(), []( int c ) {
       return (!isalnum(c) && c != '_');
     });
     auto len = iend - str.begin() - pos - 1;
@@ -354,7 +356,7 @@ OnlineConfig::OnlineConfig( const CmdLineOpts& opts )
 }
 
 //_____________________________________________________________________________
-int OnlineConfig::CheckLoadIncludeFile(
+int OnlineConfig::CheckLoadIncludeFile( // NOLINT(misc-no-recursion)
   const string& sline, const std::vector<std::string>& strvect )
 {
   if( strvect[0] == "include" ) {
@@ -385,7 +387,7 @@ int OnlineConfig::CheckLoadIncludeFile(
 // Reads in the Config File, and makes the proper calls to put
 //  the information contained into memory.
 // Returns -1 on error, otherwise the number of include files loaded (usually 0).
-int OnlineConfig::LoadFile( std::ifstream& infile, const string& filename )
+int OnlineConfig::LoadFile( std::ifstream& infile, const string& filename ) // NOLINT(misc-no-recursion)
 {
   if( !infile )
     return -1;

commit 98a6c06919c2b9b6cbe0263b9359a31fba63dc2c

Prevent ROOT GUI crash on exit
Fixes regression introduced in commit 4b22baf1 (23-Oct-22).

ROOT GUIs crashing on exit seems to be a common theme. There's practically no
documentation available on the cleanup procedure. Several hours went into trial
and error here. The common workaround seems to be to call app.Run() instead of
app.Run(true), which  will eventually just exit() instead of actually running
the destructors.

src/panguinOnline.cc
@@ -187,6 +187,7 @@ void OnlineGUI::CreateGUI( const TGWindow* p, UInt_t w, UInt_t h )
 
   // Create the main frame
   fMain = new TGMainFrame(p, w, h);
+  fMain->SetCleanup(kDeepCleanup);
   fMain->Connect("CloseWindow()", "OnlineGUI", this, "MyCloseWindow()");
   ULong_t lightgreen, lightblue, red, mainguicolor;
   gClient->GetColorByName("lightgreen", lightgreen);
@@ -1338,21 +1339,8 @@ void OnlineGUI::DeleteGUI()
 {
   DelPtr(timer);
   DelPtr(timerNow);
-  DelPtr(fPrint);
-  DelPtr(fExit);
-  DelPtr(fRunNumber);
-  DelPtr(fPrev);
-  DelPtr(fNext);
-  DelPtr(wile);
-  DelPtr(fNow);
-  DelPtr(fRootFileLastUpdated);
-  DelPtr(fPageListBox);
-  DelPtr(hframe);
-  DelPtr(fEcanvas);
-  DelPtr(vframe);
-  DelPtr(fBottomFrame);
-  DelPtr(fTopframe);
-  DelPtr(fMain);
+  fMain->Cleanup();   // Without this, ROOT will crash on exit
+  //DelPtr(fMain);    // Don't. ROOT will clean it up on exit
 }
 
 //_____________________________________________________________________________